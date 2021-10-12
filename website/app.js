ec438f5b4cf5708d314374c25a84fa64229cb400
Muhammed Ahmed Ismail <muhammadahmed.free@gmail.com>
2021-10-12 22:00:16 +0200

    client side code

--- website/app.js
+++ website/app.js
@@ -1,5 +1,64 @@
 /* Global Variables */
-
+const key = "58a6678172f10df04b2aac1cab2e7159";
+const genButton = document.querySelector("button#generate");
+const zipInput = document.querySelector("input#zip");
+const contentInput = document.querySelector("textarea#feelings");
+const dateDiv = document.querySelector("div#date");
+const tempDiv = document.querySelector("div#temp");
+const contentDiv = document.querySelector("div#content");
+//console.log(zip.value)
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+genButton.addEventListener('click', async () => {
+    let temp = await getData();
+    let content = contentInput.value;
+    //await postData(newDate, temp, content)
+    updateUI(newDate, temp, content)
+})
+
+
+function updateUI(newDate, temp, content) {
+    dateDiv.innerHTML = `Date: ${newDate}`;
+    tempDiv.innerHTML = `Temperature: ${temp} C`;
+    contentDiv.innerHTML = `How it feels: ${content}`;
+}
+
+
+
+
+
+async function getData() {
+    let zipCode = zipInput.value;
+
+    if (!zipCode || !parseInt(zipCode)) {
+        console.error("False value for zip code!!")
+        return
+    }
+    let url = `https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&units=metric&appid=${key}`;
+    console.log(url)
+    let res = await fetch(url);
+    let data = await res.json();
+    let temp = data.main.temp
+    console.log(data)
+    return temp
+
+}
+async function postData(date, temp, content) {
+    let dataTopPost = {
+        date,
+        temp,
+        content
+    }
+    await fetch("http://localhost:8000/save-data", {
+        method: "post",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(dataTopPost)
+    }).then((res) => {
+        console.log(res)
+    }).catch((err) => {
+        console.error(err)
+    })
+}
