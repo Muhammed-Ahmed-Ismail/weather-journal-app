91b2c899d36a80c9ccfa8a7f65829b9559a59605
Muhammed Ahmed Ismail <muhammadahmed.free@gmail.com>
2021-10-12 16:13:38 +0200

    adding routes

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
+const port = 8000;
 
 // Require Express to run server and routes
-
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors())
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+app.listen(port, () => {
+    console.log(`The server is running on port ${port}`);
+})
+// Routes and requestes
+app.post("/save-data", (req, res) => {
+    let data = req.body;
+    projectData.temp = data.temp;
+    projectData.date = data.date;
+    projectData.content = data.content;
+});
+app.get("/get-data", (req, res) => {
+    res.send(JSON.stringify(projectData))
+})
